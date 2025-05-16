0a0feec3958efbbdc55280ada9aa6fbb603d88df
Perdana Wahyu Jatmiko <118588435+perdanawjatmiko@users.noreply.github.com>
2025-05-16 22:23:01 +0700

    update user validation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const user = require("../controllers/userController");
 const { authenticate, authorize } = require("../middlewares/authMiddleware");
+const { updateValidation } = require('../middlewares/validators/userValidator');
+const validate = require('../middlewares/validateResult');
 
 router.get("/", authenticate, authorize(["admin", "owner", "kasir"]), user.getAll);
 router.get("/:id", authenticate, authorize(["admin", "owner"]), user.getOne);
-router.put("/:id", authenticate, authorize(["admin", "owner"]), user.update);
+router.put("/:id", authenticate, updateValidation, authorize(["admin", "owner"]), user.update);
 router.delete("/:id", authenticate, authorize(["admin"]), user.destroy);
 
 module.exports = router;
